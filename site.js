8a6bcd475956f79bd51cf0612be61ed653048d6d
Maxx Hilstad <maxxhilstad@Mac.lan>
2025-01-31 10:15:08 -0600

    Added welcome div

---
+++ site.js
@@ -0,0 +1,15 @@
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcomeMessage = document.querySelector('#welcome')
+
+if (isMorning === true) {
+    welcomeMessage.textContent = 'Good Morning!'
+} else if (isAfternoon === true) {
+    welcomeMessage.textContent = 'Good Afternoon1'
+} else {
+    welcomeMessage.textContent = 'Good Evening!'
+}
