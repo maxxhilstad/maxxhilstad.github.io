b210e6f71873ace9a5a215416e3cd03e7e414d60
Maxx Hilstad <maxxhilstad@Mac.lan>
2025-02-19 18:54:55 -0600

    Added carousel

--- site.js
+++ site.js
@@ -14,5 +14,45 @@ if (isMorning === true) {
     welcomeMessage.textContent = 'Good Evening!'
 }
 
-
 localStorage.setItem("It's a secret to everybody.", "It's dangerous to go alone! Take this.")
+
+
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+const prev = document.querySelector('#prev')
+const next = document.querySelector('#next')
+
+setInterval(() => {
+    currentImage++
+    showImages()
+}, 5000)
+
+next.addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+
+prev.addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
