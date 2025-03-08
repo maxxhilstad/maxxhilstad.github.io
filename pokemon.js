ce732bf31a6b3941e974fd21d49a9d8a6731ebad
Maxx Hilstad <maxxhilstad@Maxxs-MacBook-Pro-5.local>
2025-03-08 17:11:50 -0600

    Added pokemon and pokemon tab

--- pokemon.js
+++ pokemon.js
@@ -1,7 +1,6 @@
 (async () => {
 
-const div = document.querySelector('#pokemon')
-
+const div = document.querySelector('#pokemon') // This line returns null. Is that normal?
 
 const getRandomPokemon = async () => {
     const url = 'https://pokeapi.co/api/v2/pokemon/' + Math.floor(Math.random() * 150)
@@ -10,32 +9,25 @@ const getRandomPokemon = async () => {
     const sprite = json.sprites
     const pokemonName = json.name
 
-    const { name } = pokemonName
+    // const { name } = pokemonName
     const { front_default } = sprite
 
-    return { front_default, name }
+    return { front_default, pokemonName }
 }
 
-const renderPokemon = (pokeUrl, pokeAlt) => {
-    getRandomPokemon()
+const renderPokemon = async a => {
     const img = document.createElement('img')
-    img.src = pokeUrl// url of the image from the 'front_default' property
-    img.alt = pokeAlt// name of the pokemon
+    img.src = a.front_default // url of the image from the 'front_default' property
+    img.alt = a.pokemonName // name of the pokemon
     div.append(img)
+
+    console.log(a.front_default)
+    console.log(a.pokemonName)
 }
 
-renderPokemon("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png", 'charizard')
+const pokemon = await getRandomPokemon()
+// console.log(pokemon)
+
+await renderPokemon(pokemon)
 
 })()
-
-// There is an error that pops up when I run this:
-//
-// "Unhandled Promise Rejection: TypeError: null is not an object (evaluating 'div.append')"
-//
-// I'm trying to figure out how to display anything on the site before I move on to displaying
-// the random pokemon. I'm not sure what that error means, do you know?
-
-// Another issue I have is I'm not sure how to get the { front_default, name } values to my
-// renderPokemon function. I tried just entering renderPokemon(sprite, pokemonName) but it
-// couldn't find the variable sprite or pokemonName so I'm assuming I'm missing a step in
-// the middle. Do you have any advice on how to bridge the two? Thanks!
